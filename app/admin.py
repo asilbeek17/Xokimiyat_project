12d9ee0ef76d97b078fbcea159732305d201c08f
asilbek <asilbek1721@gmail.com>
2023-07-24 13:31:28 +0500

    models_3

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+
+admin.site.register(City)
+admin.site.register(Region)
+admin.site.register(Sanoat_loyiha)
+admin.site.register(Qishloq_xojaligi_loyiha)
+admin.site.register(Xizmat_korsatish_loyiha)
--- app/models.py
+++ app/models.py
@@ -1,22 +1,16 @@
 from django.db import models
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=155)
-
-
 class City(models.Model):
     image = models.ImageField(upload_to='City_image/')
     title = models.CharField(max_length=155)
     description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Region(models.Model):
     image = models.ImageField(upload_to='Region_image/')
     title = models.CharField(max_length=155)
     description = models.TextField
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Sanoat_loyiha(models.Model):
